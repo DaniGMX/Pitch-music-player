b5832bb02c00403dc63837065730cf61c26725c8
Daniel Alexander Gracia Machado <dgraciam@zaz-bpd5mq2.usersad.everis.int>
2020-01-13 11:25:26 +0100

    first commit

---
+++ app/src/main/java/com/danigmx/pitch/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.danigmx.pitch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
