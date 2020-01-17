254354e491061a8a7b86394bc593d6618c054280
Daniel Alexander Gracia Machado <dgraciam@bcn-6nh4y52.usersad.everis.int>
2020-01-17 11:42:08 +0100

    project structure changes

--- app/src/main/java/com/danigmx/pitch/database/dao/UsersDao.kt
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Daniel Gracia Machado on 2020-01-15.
- */
-package com.danigmx.pitch.database.dao
-
--- app/src/main/java/com/danigmx/pitch/ui/home/HomeFragment.kt
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Daniel Gracia Machado on 2020-01-13.
- */
-package com.danigmx.pitch.ui.home
-
