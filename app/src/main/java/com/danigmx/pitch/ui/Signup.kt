866f4b682c010598107ddffb9ea498164a052f62
dgraciam <danielgraciamachado@gmail.com>
2020-01-19 23:22:04 +0100

    added some files

---
+++ app/src/main/java/com/danigmx/pitch/ui/Signup.kt
@@ -0,0 +1,14 @@
+package com.danigmx.pitch.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.danigmx.pitch.R
+
+
+class Signup : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
